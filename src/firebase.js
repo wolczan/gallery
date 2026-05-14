fb0cfd3f1acca2428dd479f720246d75f604a327
Maciej Liberadzki <liberadzkimaciej@gmail.com>
2026-05-14 14:18:15 +0200

    .env creation

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,26 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import {initializeFirestore, persistentLocalCache, persistentMultipleTabManager, collection } from "firebase/firestore";
+
+import {
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+  collection,
+} from "firebase/firestore";
+
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyB5QFjl6jvIKBCRn12esrOba58STMP9dM0",
+  authDomain: "galeriazdjec-f4180.firebaseapp.com",
+  projectId: "galeriazdjec-f4180",
   storageBucket: "galeriazdjec-f4180.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  messagingSenderId: "789024046392",
+  appId: "1:789024046392:web:26d9e3195917ed28e515cd",
 };
 
-// ✅ Inicjalizacja aplikacji Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-// ✅ Poprawiona inicjalizacja Firestore **(db MUSI być zdefiniowane przed `enableIndexedDbPersistence`)**
 const db = initializeFirestore(app, {
   localCache: persistentLocalCache({
     tabManager: persistentMultipleTabManager(),
@@ -23,19 +28,9 @@ const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
 });
 
-
-// ✅ Tworzymy referencję do kolekcji "images"
 const imagesCollection = collection(db, "images");
 
-// ✅ Inicjalizacja Storage i Auth
-const storage = getStorage(app); // forceLongPolling dotyczy Firestore, nie Storage
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-// ✅ Eksportujemy poprawnie zainicjalizowane obiekty
 export { app, db, auth, storage, imagesCollection };
-
-// ✅ Logowanie konfiguracji Firebase (do debugowania)
-console.log("✅ Firebase Project ID:", firebaseConfig.projectId);
-console.log("✅ Firebase Config:", firebaseConfig);
-console.log("✅ Firebase Storage Bucket:", firebaseConfig.storageBucket);
-console.log("✅ Firestore Persistence włączone:", db);
