ec7f327d188834952a3fdb6f8bf1ab5d90427f38
Maciej Liberadzki <liberadzkimaciej@gmail.com>
2026-05-14 15:14:02 +0200

    cleanup

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,16 @@ import {
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("ALL ENV:", import.meta.env);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB5QFjl6jvIKBCRn12esrOba58STMP9dM0",
-  authDomain: "galeriazdjec-f4180.firebaseapp.com",
-  projectId: "galeriazdjec-f4180",
-  storageBucket: "galeriazdjec-f4180.firebasestorage.app",
-  messagingSenderId: "789024046392",
-  appId: "1:789024046392:web:26d9e3195917ed28e515cd",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
@@ -32,5 +35,7 @@ const imagesCollection = collection(db, "images");
 
 const auth = getAuth(app);
 const storage = getStorage(app);
+
+
 
 export { app, db, auth, storage, imagesCollection };
